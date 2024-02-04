86662ab5b48df944c04674cf7fec370e692fc54e
mintaeKim <peterzzo@naver.com>
2024-02-04 13:41:13 +0900

    240204 extension

--- src/App.js
+++ src/App.js
@@ -2,25 +2,24 @@ import styled from "styled-components";
 
 const Father = styled.div`
   display: flex;
-  background-color: black;
 `;
 const Box01 = styled.div`
-  background-color: teal;
+  background-color: ${(props) => props.bgcolor};
   width: 100px;
   height: 100px;
 `;
-const Box02 = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
+const Circle = styled(Box01)`
+  border-radius: 50px;
 `;
-
 
 function App() {
-  return <Father>
-    <Box01></Box01>
-    <Box02></Box02>
-  </Father>;
+  return (
+  <Father>
+    <Box01 bgcolor = "teal"></Box01>
+    <Box01 bgcolor = "tomato"></Box01>
+    <Circle bgcolor="black"></Circle>
+  </Father>
+  );
 }
 
 export default App;
