0b2c90a1538756fd1ff3a4e6d55c8746ca8bb00b
mintaeKim <peterzzo@naver.com>
2024-02-04 16:00:19 +0900

    theme practice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 
 const Father = styled.div`
   display: flex;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+const Myanimation = keyframes`
+  0%{
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50%{
+    transform: rotate(360deg);
+    border-radius: 50px;
+  }
+  100%{
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+`;
+
 const Box01 = styled.div`
   background-color: ${(props) => props.bgcolor};
   width: 100px;
   height: 100px;
+  animation: ${Myanimation} 5s linear infinite;
 `;
 const Circle = styled(Box01)`
   border-radius: 50px;
@@ -15,7 +35,7 @@ const Circle = styled(Box01)`
 function App() {
   return (
   <Father>
-    <Box01 bgcolor = "teal"></Box01>
+    <Title>Hello</Title>
     <Box01 bgcolor = "tomato"></Box01>
     <Circle bgcolor="black"></Circle>
   </Father>
